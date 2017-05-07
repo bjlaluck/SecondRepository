2d431600c0177f800bd792cb6309fb9988297517
Bohdan Laluck <bohdanlauck@hotmail.com>
2017-05-07 12:48:36 -0400

    Initial commit

---
+++ SecondRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondRepository
+//
+//  Created by Bohdan Laluck on 2017-05-07.
+//  Copyright © 2017 Bohdan Laluck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
